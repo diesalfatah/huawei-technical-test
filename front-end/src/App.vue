9de9f4e0b8dd4337356e03b32a073fc31a8090ec
diesalfatah <diesalfatah@gmail.com>
2026-09-21 23:09:56 +0700

    init: add frontend

---
+++ front-end/src/App.vue
@@ -0,0 +1,14 @@
+<script setup>
+import { ref } from 'vue'
+import { useCounterStore } from './stores/counter'
+
+const counter = useCounterStore()
+</script>
+
+<template>
+  <div class="min-h-screen flex items-center justify-center">
+    <button class="btn btn-primary" @click="counter.increment">
+      Count: {{ counter.count }}
+    </button>
+  </div>
+</template>
